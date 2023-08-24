4662bcafe35e03f8f25b58d30d1ee9a94574f89a
Alomatsi Akou Blandine <blandob2@gmail.com>
2023-08-24 11:24:13 +0200

    GenreController Add action

--- DineBookStore/DineBookStore/Program.cs
+++ DineBookStore/DineBookStore/Program.cs
@@ -36,6 +36,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Genre}/{action=Add}/{id?}");
 
 app.Run();
